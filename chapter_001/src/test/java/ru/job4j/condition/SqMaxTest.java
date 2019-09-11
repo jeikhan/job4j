c6adbeed2b7f1b31c3cdafabef3e1d7e08933445
jeikhan <jeikhan@yandex.ru>
2019-09-11 16:18:15 +0300

    Отладка программы в IDEA [#173963] (fixed)

--- chapter_001/src/test/java/ru/job4j/condition/SqMaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/SqMaxTest.java
@@ -8,21 +8,21 @@ public class SqMaxTest {
     @Test
     public void WhenThirdMax() {
         SqMax check = new SqMax();
-        int result = check.max(1, 2, 4, 3);
-        assertThat(result, is(4));
+        int result = check.max(3, 2, 8, 0);
+        assertThat(result, is(8));
     }
 
     @Test
     public void WhenFirstMax() {
         SqMax check = new SqMax();
-        int result = check.max(4, 1, 2, 3);
-        assertThat(result, is(4));
+        int result = check.max(8, 2, 3, 0);
+        assertThat(result, is(8));
     }
 
     @Test
     public void WhenNotMax() {
         SqMax check = new SqMax();
-        int result = check.max(4, 4, 4,4);
-        assertThat(result, is(4));
+        int result = check.max(8, 8, 8,8);
+        assertThat(result, is(8));
     }
 }
