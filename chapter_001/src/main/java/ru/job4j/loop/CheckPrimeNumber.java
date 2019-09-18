55052ea05b255c83f36c81f2c605f30de2b1a1d2
jeikhan <jeikhan@yandex.ru>
2019-09-18 15:26:49 +0300

    Простое число. [#173975] (fixed)

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,10 +3,18 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
     public boolean check(int num) {
         boolean prime;
-        if ((num %num == 0) & (num %2 == 0)) {
+
+        if (num < 2) {
             prime = false;
         } else {
             prime = true;
+        }
+
+        for (int i = 2; i <= (num / i); i++) {
+            if ((num % i) == 0) {
+                prime = false;
+                break;
+            }
         }
         return prime;
     }
