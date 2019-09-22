4b14261d70405bd8772092fbde73e14d2cb65544
jeikhan <jeikhan@yandex.ru>
2019-09-22 19:22:02 +0300

    JavaDoc. Code convention[#173970]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,5 +1,11 @@
 package ru.job4j.calculator;
-
+/**
+ * Class Calculator выполняет арифметические операции
+ * над двумя числами.
+ * @author Evgeniy Kapaev (evgenii.kapaev@yandex.ru)
+ * @since 22.09.19
+ * @version 1.0
+ */
 public class Calculator {
     public static void add(double first, double second) {
         double result = first + second;
@@ -21,6 +27,10 @@ public class Calculator {
         System.out.println(first + " - " + second + " = " + result);
     }
 
+    /**
+     * Метод main содержит методы с заданными параметрами.
+     * @param args - args
+     */
     public static void main(String[] args) {
         add(1, 1);
         div(4, 2);
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Evginiy Kapaev (evgenii.kapaev@yandex.ru)
+ * @since 22.09.19
+ * @version 1.0
+ */
+
+package ru.job4j.calculator;
