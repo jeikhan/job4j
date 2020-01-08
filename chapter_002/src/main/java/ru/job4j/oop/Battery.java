f3213eabfa2ead45a52e7d19e557720e72a4255b
jeikhan <jeikhan@yandex.ru>
2020-01-08 14:42:33 +0300

    Магические сосуды.(батарейка)[#174102]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,39 @@
+package ru.job4j.oop;
+
+/**
+ * класс списывает заряд у одной из батарей, у
+ * которой вызывают метод exchange
+ * @author Evgeniy Kapaev
+ * @version 1.0
+ */
+public class Battery {
+    private int load;
+
+    /**
+     * метод передает значение из size в load
+     * @param size - уровень заряда, который списываем у
+     *             батареи
+     */
+    public Battery(int size) {
+        this.load = size;
+    }
+
+    /**
+     * метод вычитает половину заряда у экземпляра класса,
+     * переменной которого его объявили
+     * @param another - уровень заряда баратеи, которой
+     *                приписываем заряд
+     */
+    public void exchange(Battery another) {
+        this.load = this.load - (load / 2);
+        another.load = another.load + this.load;
+    }
+
+    public static void main(String[] args) {
+        Battery first = new Battery(50);
+        Battery second = new Battery(10);
+        System.out.println("first battery: " + first.load + ". second: " + second.load);
+        second.exchange(first);
+        System.out.println("first battery: " + first.load + ". second: " + second.load);
+    }
+}
