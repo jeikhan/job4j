33891fe2ca95672593a01aeb68a9be7639ad00c4
jeikhan <jeikhan@yandex.ru>
2020-02-22 18:50:18 +0300

    Переопределение [#174890]

---
+++ chapter_002/src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -0,0 +1,13 @@
+package ru.job4j.inheritance;
+
+public class JSONReport extends TextReport {
+    public String generate(String name, String body) {
+        return "{" +
+                " <br/> " +
+                "name: " + name +
+                " <br/> " +
+                "body: " + body +
+                " <br> " +
+                "}";
+    }
+}
