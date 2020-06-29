39bb0cc289b80a0ad999214cc05cad198f179c1b
jeikhan <jeikhan@yandex.ru>
2020-06-29 16:46:31 +0300

    Доработать тесты в StartUITest проверяющие вывод данных в консоль.[33585#174075]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -2,11 +2,28 @@ package ru.job4j.tracker;
 
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
-import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.*;
 
 public class StartUITest {
 
+    @Test
+    public void whenCreate() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"1"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new CreateAction(out),
+                new ExitAction()
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                "======== Menu ========" + System.lineSeparator() +
+                        "0. Create new item" + System.lineSeparator() +
+                        "1. Exit" + System.lineSeparator()
+        ));
+    }
 
     @Test
     public void whenExit() {
