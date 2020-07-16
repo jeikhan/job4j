afa2f396b1cb077d949fca2642af0f8ee16f954c
jeikhan <jeikhan@yandex.ru>
2020-07-16 06:57:38 +0300

    Доработать тесты в StartUITest проверяющие вывод данных в консоль.[33585#174075]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -7,6 +7,23 @@ import static org.junit.Assert.*;
 public class StartUITest {
 
     @Test
+    public void whenFindAll() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new ShowAllAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                "Menu:" + System.lineSeparator() +
+                        "0. Show all items" + System.lineSeparator()
+        ));
+    }
+
+    @Test
     public void whenExit() {
         Output out = new StubOutput();
         Input in = new StubInput(
