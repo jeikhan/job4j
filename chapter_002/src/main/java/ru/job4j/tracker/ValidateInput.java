1cf6aed364551ff1a8021d0460a69dd03523be79
jeikhan <jeikhan@yandex.ru>
2020-08-05 14:38:33 +0300

    Рефакторинг - Шаблон Декоратор для валидатора. [#34117]

--- chapter_002/src/main/java/ru/job4j/tracker/ValidateInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -4,20 +4,30 @@ package ru.job4j.tracker;
  * Обеспечение бесперебойной работы приложения
  * на ввод несуществующего пункта меню и ввод
  * строки вместо числа.
- * @author Evgenii Kapaev
  * @version 1.0
  */
-public class ValidateInput extends ConsoleInput {
+public class ValidateInput implements Input {
+    private final Output out;
+    private final Input in;
+
+    public ValidateInput(Output out, Input input) {
+        this.out = out;
+        this.in = input;
+    }
+
+    @Override
+    public String askStr(String question) {
+        return in.askStr(question);
+    }
+
     @Override
     public int askInt(String question) {
         boolean invalid = true;
         int value = -1;
         do {
             try {
-                value = super.askInt(question);
+                value = in.askInt(question);
                 invalid = false;
-            } catch (IllegalStateException moe) {
-                System.out.println("Please select key from menu.");
             } catch (NumberFormatException nfe) {
                 System.out.println("Please enter validate data again.");
             }
