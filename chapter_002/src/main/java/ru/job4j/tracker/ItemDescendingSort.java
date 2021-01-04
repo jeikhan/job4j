13ddfbfe7783f1ecc90fb6d3cb8c0e33b0d933bb
jeikhan <jeikhan@yandex.ru>
2021-01-04 22:07:44 +0300

    add Override hashcode & equals

--- chapter_002/src/main/java/ru/job4j/tracker/ItemDescendingSort.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ItemDescendingSort.java
@@ -3,9 +3,18 @@ package ru.job4j.tracker;
 import java.util.Comparator;
 
 public class ItemDescendingSort implements Comparator<Item> {
-
     @Override
     public int compare(Item o1, Item o2) {
-        return o1.getName().compareTo(o2.getName());
+        return o2.getName().compareTo(o1.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
     }
 }
