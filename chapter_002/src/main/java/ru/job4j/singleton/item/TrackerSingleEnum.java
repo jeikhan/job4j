5d0c6cc81d7602258024239274c7a244e7e80a7f
jeikhan <jeikhan@yandex.ru>
2021-01-05 21:20:02 +0300

    Move item package from tracker to singleton

--- chapter_002/src/main/java/ru/job4j/singleton/item/TrackerSingleEnum.java
+++ chapter_002/src/main/java/ru/job4j/singleton/item/TrackerSingleEnum.java
@@ -1,4 +1,4 @@
-package ru.job4j.tracker.item;
+package ru.job4j.singleton.item;
 
 import ru.job4j.tracker.Item;
 
--- chapter_002/src/test/java/ru/job4j/singleton/item/TrackerSingleTest.java
+++ chapter_002/src/test/java/ru/job4j/singleton/item/TrackerSingleTest.java
@@ -1,4 +1,4 @@
-package ru.job4j.tracker.item;
+package ru.job4j.singleton.item;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
