2f05621cfd8a676a11b20852607fafdf682422a4
Evgen Kapaev <jeikhan@yandex.ru>
2021-03-04 20:50:09 +0300

    Организовать сортировку User[10034#174039]

---
+++ chapter_003/src/main/java/ru/job4j/collect/User.java
@@ -0,0 +1,44 @@
+package ru.job4j.collect;
+
+import java.util.Objects;
+
+/**
+ * Задание: организовать сортировку
+ * по модели User(имя и возраст).
+ * @author Evgen Kapaev
+ */
+public class User implements Comparable<User> {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(User o) {
+        if (o.name == name) {
+        return Integer.compare(age, o.age);
+        } else {
+            return name.compareTo(o.name);
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return age == user.age && Objects.equals(name, user.name);
+    }
+}
