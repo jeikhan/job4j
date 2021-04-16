0b5598820044a04399b01c8e990789ba7d7775b4
Evgen Kapaev <jeikhan@yandex.ru>
2021-04-16 15:40:07 +0300

    Зона видимости в лямбда и исключения[70609#173927]

---
+++ chapter_004/src/main/java/ru/job4j/lambda/ScopeInside.java
@@ -0,0 +1,27 @@
+package ru.job4j.lambda;
+
+import java.util.function.Supplier;
+
+/**
+ * Задание: Исправить ошибку в коде, который
+ * складывает числа массива.
+ * @author Evgen Kapaev
+ */
+public class ScopeInside {
+    public static void main(String[] args) {
+        int[] number = {1, 2, 3};
+        int total = 0;
+        for (int i = 0; i < number.length; i++) {
+            int num = number[i];
+            int sum = total;
+            total = add(
+                    () -> sum + num
+            );
+        }
+        System.out.println(total);
+    }
+
+    private static Integer add(Supplier<Integer> calc) {
+        return calc.get();
+    }
+}
