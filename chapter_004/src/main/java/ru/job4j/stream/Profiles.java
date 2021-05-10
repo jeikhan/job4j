183a284e21c908b95d5d46266bd8c70a93f0b0be
Evgen Kapaev <jeikhan@yandex.ru>
2021-05-10 20:54:27 +0300

    Уникальность элементов и сортировка. [#110225]
    - в классе Profiles дописаны сортировка элементов по полю "City" и добавлен метод исключающий повторяющиеся адреса;
    - дописан test-class ProfilesTest на методы sorted(), distinct().

--- chapter_004/src/main/java/ru/job4j/stream/Profiles.java
+++ chapter_004/src/main/java/ru/job4j/stream/Profiles.java
@@ -1,11 +1,14 @@
 package ru.job4j.stream;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 /**
  * Задание: Составить список адресов клиентов,
  * в анкете клиента есть адрес проживания.
+ * Update: добавлена сортировка и исключение
+ * дубликатов адресов.
  * @author Evgenii Kapaev
  * @since 10.05.21
  */
@@ -14,6 +17,8 @@ public class Profiles {
         return profiles
                 .stream()
                 .map(Profile::getAddress)
+                .sorted(Comparator.comparing(Address::getCity))
+                .distinct()
                 .collect(Collectors.toList());
     }
 }
--- chapter_004/src/test/java/ru/job4j/stream/ProfilesTest.java
+++ chapter_004/src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 public class ProfilesTest {
     @Test
@@ -18,8 +18,50 @@ public class ProfilesTest {
         ));
         List<Address> expected = List.of(
                 new Address("Moscow", "Krasnopresnenskaya", 2, 1),
-                new Address("Sankt-Peterburg", "Vysotnaya", 1, 0),
-                new Address("Omsk", "Krachy Put", 1, 1)
+                new Address("Omsk", "Krachy Put", 1, 1),
+                new Address("Sankt-Peterburg", "Vysotnaya", 1, 0)
+        );
+        assertThat(list, is(expected));
+    }
+
+    @Test
+    public void sortingAddress() {
+        Profiles profiles = new Profiles();
+        List<Address> list = profiles.collect(List.of(
+                new Profile(new Address("Moscow", "Krasnopresnenskaya", 2, 1)),
+                new Profile(new Address("Sankt-Peterburg", "Vysotnaya", 1, 0)),
+                new Profile(new Address("Omsk", "Krachy Put", 1, 1)),
+                new Profile(new Address("Novosibirsk", "Sibirskaya", 1, 0)),
+                new Profile(new Address("Kaliningrad", "Evropeiskaya", 1, 1))
+        ));
+        List<Address> expected = List.of(
+                new Address("Kaliningrad", "Evropeiskaya", 1, 1),
+                new Address("Moscow", "Krasnopresnenskaya", 2, 1),
+                new Address("Novosibirsk", "Sibirskaya", 1, 0),
+                new Address("Omsk", "Krachy Put", 1, 1),
+                new Address("Sankt-Peterburg", "Vysotnaya", 1, 0)
+        );
+        assertThat(list, is(expected));
+    }
+
+    @Test
+    public void distinctAddress() {
+        Profiles profiles = new Profiles();
+        List<Address> list = profiles.collect(List.of(
+                new Profile(new Address("Moscow", "Krasnopresnenskaya", 2, 1)),
+                new Profile(new Address("Sankt-Peterburg", "Vysotnaya", 1, 0)),
+                new Profile(new Address("Omsk", "Krachy Put", 1, 1)),
+                new Profile(new Address("Novosibirsk", "Sibirskaya", 1, 0)),
+                new Profile(new Address("Kaliningrad", "Evropeiskaya", 1, 1)),
+                new Profile(new Address("Novosibirsk", "Sibirskaya", 1, 0)),
+                new Profile(new Address("Sankt-Peterburg", "Vysotnaya", 1, 0))
+        ));
+        List<Address> expected = List.of(
+                new Address("Kaliningrad", "Evropeiskaya", 1, 1),
+                new Address("Moscow", "Krasnopresnenskaya", 2, 1),
+                new Address("Novosibirsk", "Sibirskaya", 1, 0),
+                new Address("Omsk", "Krachy Put", 1, 1),
+                new Address("Sankt-Peterburg", "Vysotnaya", 1, 0)
         );
         assertThat(list, is(expected));
     }
