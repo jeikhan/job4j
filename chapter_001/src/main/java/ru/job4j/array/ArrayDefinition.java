6ff817a6f7a7b663ca6c1a397c30fab0d3883f3c
Evgen Kapaev <jeikhan@yandex.ru>
2021-05-23 16:42:25 +0300

    Массивы и цикл for. [#226665 #159652]
    - исправлено по условию задания.

--- chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -2,13 +2,12 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        String[] names = new String[4];
-        names[0] = "Вася";
-        names[1] = "Паша";
-        names[2] = "Саша";
-        names[3] = "Валя";
-        for (int i = 0; i < 4; i++) {
-            System.out.println(names[i]);
+        int[] numbers = new int[5];
+        for (int index = 0; index < numbers.length; index++) {
+            numbers[index] = index * 2 + 3;
+        }
+        for (int index = 0; index < numbers.length; index++) {
+            System.out.print(numbers[index] + " ");
         }
     }
 }
