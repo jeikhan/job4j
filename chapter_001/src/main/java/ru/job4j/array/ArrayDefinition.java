d8f3930d90d7b5daf31e42738b6f3e5afc34ac67
Evgen Kapaev <jeikhan@yandex.ru>
2021-05-23 13:48:32 +0300

    Заполнение массива. [#226300]

--- chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -2,11 +2,13 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
-        System.out.println("Размер массива ages: " + ages.length);
-        String[] surnames = new String[100500];
-        System.out.println("Размер массива surnames: " + surnames.length);
-        float[] prices = new float[40];
-        System.out.println("Размер массива prices: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Вася";
+        names[1] = "Паша";
+        names[2] = "Саша";
+        names[3] = "Валя";
+        for (int i = 0; i < 4; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
