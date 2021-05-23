d34eabf14bd9c5e375b5da848cd6b179e1cb43ec
Evgen Kapaev <jeikhan@yandex.ru>
2021-05-23 12:38:14 +0300

    Math.abs [#373616 #159498]
    - изменил условие проверки координат.

--- chapter_001/src/main/java/ru/job4j/condition/ChessBoard.java
+++ chapter_001/src/main/java/ru/job4j/condition/ChessBoard.java
@@ -3,27 +3,12 @@ package ru.job4j.condition;
 public class ChessBoard {
     public static int way(int x1, int y1, int x2, int y2) {
         int result = 0;
-        if (Math.abs(x2 - x1) == Math.abs(y2 - y1)) {
-            if (checkCoordinates(x1, y1, x2, y2)) {
+        if (
+                x1 >= 0 && x1 <= 7 && x2 >= 0 && x2 <= 7
+                        && y1 >= 0 && y1 <= 7 && y2 >= 0 && y2 <= 7
+        ) {
+            if (Math.abs(x2 - x1) == Math.abs(y2 - y1)) {
                 result = Math.abs(x2 - x1);
-            }
-        }
-        return result;
-    }
-
-    public static boolean checkCoordinates(int x1, int y1, int x2, int y2) {
-        boolean result = false;
-        int[] coordinates = new int[4];
-        coordinates[0] = x1;
-        coordinates[1] = y1;
-        coordinates[2] = x2;
-        coordinates[3] = y2;
-        for (int num : coordinates) {
-            if (num >= 0 && num <= 7) {
-                result = true;
-            } else {
-                result = false;
-                break;
             }
         }
         return result;
