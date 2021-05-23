8350c0b1aa60c0b80c673c37fbdc6899a9a8a06c
Evgen Kapaev <jeikhan@yandex.ru>
2021-05-23 14:27:26 +0300

    Массивы в тестах. [#226789]

--- chapter_001/src/main/java/ru/job4j/array/Square.java
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -22,4 +22,11 @@ public class Square {
         }
         return result;
     }
+
+    public static void main(String[] args) {
+        int[] array = calculate(4);
+        for (int index = 0; index < array.length; index++) {
+            System.out.println(array[index]);
+        }
+    }
 }
--- chapter_001/src/test/java/ru/job4j/array/SquareTest.java
+++ chapter_001/src/test/java/ru/job4j/array/SquareTest.java
@@ -1,34 +1,22 @@
 package ru.job4j.array;
 
+import org.junit.Assert;
 import org.junit.Test;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
 
 public class SquareTest {
     @Test
     public void whenBound3Then149() {
         int bound = 3;
-        Square square = new Square();
         int[] rst = Square.calculate(bound);
-        int[] expect = new int[] {1, 4, 9};
-        assertThat(rst, is(expect));
+        int[] expect = new int[]{1, 4, 9};
+        Assert.assertArrayEquals(rst, expect);
     }
 
     @Test
-    public void whenBound6Then149162536() {
-        int bound = 6;
-        Square square = new Square();
+    public void whenBound6Then1491625() {
+        int bound = 5;
         int[] rst = Square.calculate(bound);
-        int[] expect = new int[] {1, 4, 9, 16, 25, 36};
-        assertThat(rst, is(expect));
-    }
-
-    @Test
-    public void whenBound10Then149162536496481100() {
-        int bound = 10;
-        Square square = new Square();
-        int[] rst = Square.calculate(bound);
-        int[] expect = new int[] {1, 4, 9, 16, 25, 36, 49, 64, 81, 100};
-        assertThat(rst, is(expect));
+        int[] expect = new int[]{1, 4, 9, 16, 25};
+        Assert.assertArrayEquals(rst, expect);
     }
 }
