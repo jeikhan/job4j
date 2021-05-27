6d046d8c5d98c72759ab7033007852feba9d683a
Evgen Kapaev <jeikhan@yandex.ru>
2021-05-27 16:40:38 +0300

    Переопределение [#174890]

--- chapter_002/src/main/java/ru/job4j/inheritance/JSONReport.java
+++ chapter_002/src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -16,18 +16,8 @@ public class JSONReport extends TextReport {
      */
     @Override
     public String generate(String name, String body) {
-        return "{"
-                +
-                " <br/> "
-                +
-                "name: " + name
-                +
-                " <br/> "
-                +
-                "body: " + body
-                +
-                " <br> "
-                +
-                "}";
+        return  "name : " + name
+                + System.lineSeparator()
+                + "body : " + body;
     }
 }
