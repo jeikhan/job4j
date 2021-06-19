01ff74ff15c959685a43e71039b3ba95ef6bf212
Evgen Kapaev <jeikhan@yandex.ru>
2021-06-19 15:02:40 +0300

    Приведение типов

--- chapter_002/src/main/java/ru/job4j/oop/College.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j.oop;
-
-public class College {
-    public static void main(String[] args) {
-        Students students = new Students();
-        Freshman freshman = students;
-        Object obj = freshman;
-    }
-}
