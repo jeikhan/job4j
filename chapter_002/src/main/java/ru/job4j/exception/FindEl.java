3f9c86c43e618b83f60e74f39ea1991a14e42023
Evgen Kapaev <jeikhan@yandex.ru>
2021-06-21 14:06:20 +0300

    Пользовательские исключения. [#219367]

---
+++ chapter_002/src/main/java/ru/job4j/exception/FindEl.java
@@ -0,0 +1,24 @@
+package ru.job4j.exception;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int index = 0; index < value.length; index++) {
+            if (key.equals(value[index])) {
+                rsl = index;
+            } else {
+                throw new ElementNotFoundException("User not found!");
+            }
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String[] users = {"May", "Hammond", "Clarkson"};
+        try {
+            indexOf(users, "Stig");
+        } catch (ElementNotFoundException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
