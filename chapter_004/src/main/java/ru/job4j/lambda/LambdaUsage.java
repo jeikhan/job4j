1251a79fd6de5199e10634c8556db486cd43e5e4
Evgen Kapaev <jeikhan@yandex.ru>
2021-08-19 15:02:28 +0300

    Лямбда блок #249208
    - добавлена демонстрация сортировки написанным ранее компаратором.

--- chapter_004/src/main/java/ru/job4j/lambda/LambdaUsage.java
+++ chapter_004/src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -1,12 +1,16 @@
 package ru.job4j.lambda;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 public class LambdaUsage {
     public static void main(String[] args) {
+        String[] strings = {"Hammond", "Clarkson", "May", "Stig"};
         Comparator<String> comparator = (left, right) -> {
             System.out.println("compare - " + left + " : " + right);
             return Integer.compare(right.length(), left.length());
         };
+        Arrays.sort(strings, comparator);
+        System.out.println("result - " + Arrays.toString(strings));
     }
 }
