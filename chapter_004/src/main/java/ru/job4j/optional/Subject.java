03d40eb9f0058129dde7b9b57fa05434dcde39df
Evgen Kapaev <jeikhan@yandex.ru>
2021-09-09 20:40:43 +0300

    Optional в банковских переводах. #214711

---
+++ chapter_004/src/main/java/ru/job4j/optional/Subject.java
@@ -0,0 +1,42 @@
+package ru.job4j.optional;
+
+import java.util.Objects;
+
+/**
+ * Модель дисциплины.
+ */
+public class Subject {
+    private String name;
+
+    private int score;
+
+    public Subject(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Subject)) {
+            return false;
+        }
+        Subject subject = (Subject) o;
+        return Objects.equals(name, subject.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
